5adb1aee5014cbb4476b61144551b1cdbd50e3c8
RajveerNarang <dhanurrajveer@gmail.com>
2023-05-17 17:27:26 +0530

    chart card UI

--- Howdy-Chat/src/components/ChatCardsListing/ChatCardsListing.jsx
+++ Howdy-Chat/src/components/ChatCardsListing/ChatCardsListing.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import "./ChatCardsListing.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const ChatCardsListing = () => {
   return (
-    <div>
-      <h1>ChatCardsListing</h1>
+    <div className="chat-cards-listing">
+      {/* <h1>ChatCardsListing</h1> */}
+      <div className="card">
+        <div className="img-container">
+          <img
+            alt="card-icon"
+            src="https://posts-cdn.kueez.net/OAyTRw3lzuXbeail/image-PsVOkM5mC70yIrht.jpg"
+          />
+        </div>
+        <div className="card-detail">
+          <h4 className="title">User</h4>
+          <p className="desc">Hey there!!!</p>
+        </div>
+        <div className="time">5:00PM</div>
+        <div className="action-btn">
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+      </div>
+      <div className="card">
+        <div className="img-container">
+          <img
+            alt="card-icon"
+            src="https://posts-cdn.kueez.net/OAyTRw3lzuXbeail/image-PsVOkM5mC70yIrht.jpg"
+          />
+        </div>
+        <div className="card-detail">
+          <h4 className="title">User</h4>
+          <p className="desc">Hey there!!!</p>
+        </div>
+        <div className="time">5:00PM</div>
+        <div className="action-btn">
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+      </div>
     </div>
   );
 };
